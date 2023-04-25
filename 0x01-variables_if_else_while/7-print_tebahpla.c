007af24b3135e2d55acdcaa2b24d9cf4fb654dc4
Carla Bothma <maximum.impact0305@gmail.com>
2023-04-24 19:52:20 -0700

    Alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in reverse and lowercase.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
