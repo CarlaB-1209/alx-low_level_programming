86c391cab62529d7a4c158bfdf644fcf528f13b5
Carla Bothma <maximum.impact0305@gmail.com>
2023-04-26 12:33:26 -0700

    Alphabet 10 times new take

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,29 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - prints the lowercase alphabet ten times
+ * print_alphabet_x10 - prints the lowercase alphabet
  */
 
 void print_alphabet_x10(void)
 {
-	int arr[26] = {a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z};
+	char char;
 
 	int i;
 
-	for (i = 1; i <=10; i++)
-		_putchar(arr);
-	_putchar('\n');
+	i = 0;
 
+	while (i <10)
+	{
+		ch = 'a';
+
+		while (ch <='z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+
+		_putchar('\n');
+
+		i++;
+	}
 }
