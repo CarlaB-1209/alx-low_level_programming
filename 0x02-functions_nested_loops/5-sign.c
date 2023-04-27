28839bc297fb306ce3296fea787acf2fddba7cdb
Carla Bothma <maximum.impact0305@gmail.com>
2023-04-26 19:26:51 -0700

    Checks sign of an integer

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of an integer
+ * @n: the integer to be checked
+ * Return: 1 if n is greater than zero
+ * 0 if n is zero
+ * -1 if n is greater than zero
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+		_putchar('+');
+		return (1);
+	
+	else if (n == 0)
+		_putchar('0');
+		return (0);
+
+	else if (n < 0);
+		_putchar('-');
+		return (-1);
+}
