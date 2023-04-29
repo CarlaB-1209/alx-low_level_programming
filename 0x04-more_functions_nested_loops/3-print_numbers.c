b1c69e9d10222e09640c17566517f306ac678801
Carla Bothma <maximum.impact0305@gmail.com>
2023-04-29 13:02:16 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers 0 - 9
+ * @c: char to be printed
+ * Return: 0 
+ */
+
+void print_numbers(void)
+{
+	char c;
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+
+	_putchar('\n');
+}
