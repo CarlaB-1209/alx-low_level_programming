8c3f5c8692f5f5122a57caaf61ac5448b8e283c5
Carla Bothma <maximum.impact0305@gmail.com>
2023-04-29 19:42:49 -0700

    All edits

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- * more_numbers - prints numbers 0 to 14 ten times
+ * more_numbers - print more numbers
  */
 
 void more_numbers(void)
-
 {
 	int i, j;
 
@@ -14,11 +13,9 @@ void more_numbers(void)
 		for (j = 0; j <= 14; j++)
 		{
 			if (j >= 10)
-				_putchar('1);
-				_putchar(j % 10 + '0');
+				_putchar('1');
+			_putchar (j % 10 + '0');
 		}
-
 		_putchar('\n');
 	}
 }
-
