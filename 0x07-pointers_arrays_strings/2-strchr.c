b256eb9d28d908a8ca09e6e4b46d74b63a97ca5e
Carla Bothma <maximum.impact0305@gmail.com>
2023-05-09 11:15:56 -0700

    Search for a character edit

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -14,12 +14,12 @@ char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	for (i = 0; s[i] >= '\0'; i++)
+	for (; s[i] >= '\0'; i++)
 	{
 		if (s[i] == c)
 			return (&s[i]);
-
-		else
-			return (0);
 	}
+
+	return (0);
 }
+
