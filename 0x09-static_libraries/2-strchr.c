9c53e7f9ba63fda5d6761d7861d40f8177cfccac
Carla Bothma <maximum.impact0305@gmail.com>
2023-05-17 04:28:29 -0700

    Static library creation

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *_strchr - locates a character in a string
+ * @s: input string being investigated
+ * @c: input character being searched for
+ *
+ * Return: a pointer to the first occurrence of the character c
+ * or NULL if character not found
+ */
+
+char *_strchr(char *s, char c)
+
+{
+	int i = 0;
+
+	for (; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+
+	return (0);
+}
---
+++ 0x09-static_libraries/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: input string being analyzed
+ * @accept: template input
+ *
+ * Return:  a pointer to the byte in s that matches accept
+ * or NULL if no such byte is found
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int k;
+
+	while (*s)
+	{
+		for (k = 0; accept[k]; k++)
+		{
+			if (*s == accept[k])
+				return (s);
+		}
+		s++;
+	}
+
+	return ('\0');
+}
