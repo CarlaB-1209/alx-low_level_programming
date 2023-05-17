5da8f0c1c29bc51c30ab38290de9a6391b4c3aaa
Carla Bothma <maximum.impact0305@gmail.com>
2023-05-17 12:54:45 -0700

    Make a grid

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,19 +13,24 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *conct;
-
 	int i;
-
 	int ci;
 
 	if (s1 == NULL)
 		s1 = "";
-
 	if (s2 == NULL)
 		s2 = "";
 
 	i = ci = 0;
+	while (s1[i] != '\0')
+		i++;
+	while (s2[ci] != '\0')
+		ci++;
+	conct = malloc(sizeof(char) * (i + ci + 1));
 
+	if (conct == NULL)
+		return (NULL);
+	i = ci = 0;
 	while (s1[i] != '\0')
 	{
 		conct[i] = s1[i];
@@ -38,8 +43,7 @@ char *str_concat(char *s1, char *s2)
 		i++;
 		ci++;
 	}
-
 	conct[i] = '\0';
-
 	return (conct);
 }
+
