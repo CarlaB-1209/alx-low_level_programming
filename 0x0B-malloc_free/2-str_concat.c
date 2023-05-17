18fd2ef4e24de6890af8218f95ec9385a5487214
Carla Bothma <maximum.impact0305@gmail.com>
2023-05-17 12:28:33 -0700

    Links two strings

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * str_concat - links two strings and assigns appropriate memory space
+ * @s1: input string 1
+ * @s2: input string 2
+ * Return: pointer to concatenated strings, terminate on NULL, NULL on failure
+ *
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *conct;
+
+	int i;
+
+	int ci;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	i = ci = 0;
+
+	while (s1[i] != '\0')
+	{
+		conct[i] = s1[i];
+		i++;
+	}
+
+	while (s2[ci] != '\0')
+	{
+		conct[i] = s2[ci];
+		i++;
+		ci++;
+	}
+
+	conct[i] = '\0';
+
+	return (conct);
+}
