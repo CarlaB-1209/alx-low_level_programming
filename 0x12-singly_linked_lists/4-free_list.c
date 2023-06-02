7238eec7c3b7b2d8542de479fd72225268f43e42
CarlaB-1209 <maximum.impact0305@gmail.com>
2023-06-01 19:05:58 -0700

    Frees a linked list task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - frees a linked list
+ * @head: list that will be freed
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+
+		free(head->str);
+		free(head);
+
+		head = temp;
+	}
+}
