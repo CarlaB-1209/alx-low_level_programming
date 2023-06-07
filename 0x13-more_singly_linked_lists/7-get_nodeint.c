f89db0bee084dc00f562d4853fbba2cd96fb063c
CarlaB-1209 <maximum.impact0305@gmail.com>
2023-06-07 12:42:55 -0700

    Task 7 updated

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -5,7 +5,7 @@
  * @head: first node in the linked list
  * @index: index of the node to return
  *
- * Return: pointer to the node we're looking for, or NULL
+ * Return: pointer to the specified node (success) or NULL
  */
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
