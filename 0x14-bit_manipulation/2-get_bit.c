000bc3f1620395c89670108318ac732951ac5b03
Carla Bothma <maximum.impact0305@gmail.com>
2023-07-06 20:24:27 -0700

    Task 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * get_bit - returns the vlaue ofa bit at a secified index
+ * @n: input number
+ * @index: specified index
+ * Return: value of bit (success) or -1 (error)
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int bit_number = sizeof(unsigned long int) * 8;
+	unsigned int important_bit;
+	int bit_value;
+
+	if (index >= bit_number)
+		return (-1);
+
+	important_bit = 1 << index;
+
+	bit_value = (n & important_bit) != 0;
+
+	return (bit_value);
+}
